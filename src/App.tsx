2480fff38fda4f5b5c9ebda6d84084edc7a961ea
内藤祐馬 <naitoyuma@outlook.jp>
2023-02-07 11:23:26 +0900

    prettier eslint

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { useState } from "react";
 // Todoオブジェクトの型定義
 type Todo = {
 	value: string,
-	readonly id: number
+	readonly id: number,
+	checked: boolean,
+	removed: boolean;
 };
 
 export const App = () => {
@@ -11,17 +13,25 @@ export const App = () => {
 	// [value, action] = useState('initValue')
 	const [text, setText] = useState('');
 
-	const [todos, setTodos] = useState<Todo[]>([]);
+	const [todos, setTodos] = useState<Todo[]>([{
+		value: 'HELLO',
+		id: 11,
+		checked: false,
+		removed: false
+	}]);
 
 	const handleSubmit = () => {
 		if (!text) return
 		
 		const newTodo: Todo = {
 			value: text,
-			id: new Date().getTime()
+			id: new Date().getTime(),
+			checked: false,
+			removed: false
 		}
 
 		// 値の更新で破壊的な処理を行わない
+		// スプレッドは非破壊的なディープコピー(ネストされた参照先も変更される)
 		setTodos([newTodo, ...todos]);
 		setText("");
 	}
@@ -29,7 +39,55 @@ export const App = () => {
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setText(e.target.value);
 	};
-	
+
+	const handleEdit = (id: number, value: string) => {
+
+		//.map()は非破壊的ではないが、ネストしたプロパティはコピーされずの参照先は変わらない
+		//.map()で参照するプロパティに対する変更はオリジナルへのミューテート扱いとなる
+
+		// そのため.mapをスプレッドで展開しディープコピーする
+		const deepCopy = todos.map((todo) => ({ ...todo }));
+
+		const newTodos = deepCopy.map((todo) => {
+			if (todo.id === id) {
+				todo.value = value;
+			}
+			return todo;
+		})
+
+		// todos ステートが書き換えられていないかチェック
+		console.log('=== Original todos ===');
+		todos.map((todo) => console.log(`id: ${todo.id}, value: ${todo.value}`));
+
+		setTodos(newTodos);
+	}
+
+	const handleCheck = (id: number, checked:boolean) => {
+		const deepCopy = todos.map((todo) => ({ ...todo }));
+
+		const newTodos = deepCopy.map((todo) => {
+			if (todo.id === id ) {
+				todo.checked = !checked;
+			}
+			return todo;
+		})
+
+		setTodos(newTodos);
+	}
+
+	const handleDelete = (id: number,  removed: boolean) => {
+		const deepCopy = todos.map((todo) => ({ ...todo }));
+
+		const newTodos = deepCopy.map((todo) => {
+			if (todo.id === id) {
+				todo.removed = !removed;
+			}
+			return todo;
+		})
+
+		setTodos(newTodos);
+	}
+
 	return (
 		<div>
 			<form onSubmit={(e) => {
@@ -50,8 +108,32 @@ export const App = () => {
 			</form>
 		<ul>
 				{todos.map((todo) => {
-					return <li>{todo.value}</li>
+					return (
+						<li key={todo.id}>
+							<input
+								type="checkbox"
+								checked={todo.checked}
+								disabled={todo.removed}
+								onChange={() => handleCheck(
+									todo.id,
+									todo.checked
+								)}
+							/>
+							<input
+								type="text"
+								disabled={todo.checked || todo.removed}
+								value={todo.value}
+								onChange={(e) => {
+									handleEdit(todo.id, e.target.value)
+								}}
+							/>
+							<button onClick={() => handleDelete(todo.id, todo.removed)}>
+								{todo.removed ? '復元' : '削除'}
+							</button>
+						</li>
+					)
 				})}
+				
 		</ul>
 		</div>
 	);
